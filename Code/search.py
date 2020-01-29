0a63bde54915bbd68d2f2c7f4a2286374ab03238
Christopher Barnes <cbarnes7404@yahoo.com>
2020-01-29 09:22:49 -0800

    add recursive search

--- Code/search.py
+++ Code/search.py
@@ -4,8 +4,8 @@ def linear_search(array, item):
     """return the first index of item in array or None if item is not found"""
     # implement linear_search_iterative and linear_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    return linear_search_iterative(array, item)
-    # return linear_search_recursive(array, item)
+    # return linear_search_iterative(array, item)
+    return linear_search_recursive(array, item)
 
 
 def linear_search_iterative(array, item):
@@ -18,9 +18,12 @@ def linear_search_iterative(array, item):
 
 def linear_search_recursive(array, item, index=0):
     # TODO: implement linear search recursively here
-    pass
-    # once implemented, change linear_search to call linear_search_recursive
-    # to verify that your recursive implementation passes all tests
+    # print('index: ', index, 'preview: ', array[index], 'target: ', item)
+    if item == array[index]:
+        return index
+    else:
+        nxt = index + 1
+        return linear_search_recursive(array, item, nxt)
 
 
 def binary_search(array, item):
@@ -43,3 +46,11 @@ def binary_search_recursive(array, item, left=None, right=None):
     pass
     # once implemented, change binary_search to call binary_search_recursive
     # to verify that your recursive implementation passes all tests
+
+if __name__ == "__main__":
+    """Read command-line arguments and convert given digits between bases."""
+    import sys
+    args = sys.argv[1:]  # Ignore script file name
+    
+    arr = [5,3,4,2,6,1]
+    print('index: ', linear_search(arr, 2))
